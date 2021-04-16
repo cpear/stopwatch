0eaca23e779e915f87e8814da1ec655e9a343c31
Matthew Richardson <matthew@velocitysystems.co.nz>
2021-04-16 11:50:10 +0100

    Implement stopwatch demo app with and without data-binding.

--- Stopwatch/MainPage.xaml.cs
+++ Stopwatch/MainPage.xaml.cs
@@ -1,18 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using Xamarin.Forms;
 
 namespace Stopwatch
 {
     public partial class MainPage : ContentPage
     {
+        private const int IntervalMilliseconds = 100;
+
+        private Timer _timer;
+        private TimeSpan _time;
+
         public MainPage()
         {
             InitializeComponent();
+            
+            // This is required for the data-binding example.
+            BindingContext = new ViewModel();
+        }
+
+        void Start_Clicked(object sender, EventArgs e)
+        {
+            if (_timer != null)
+            {
+                _timer.Stop();
+                _timer.Elapsed -= Timer_Elapsed;
+            }
+
+            _timer = new Timer(IntervalMilliseconds);
+            _timer.Elapsed += Timer_Elapsed;
+            _timer.Start();
+        }
+
+        void Stop_Clicked(object sender, EventArgs e)
+        {
+            if (_timer is null)
+            {
+                return;
+            }
+
+            _timer.Stop();
+        }
+
+        void Reset_Clicked(object sender, EventArgs e)
+        {
+            if (_timer is null)
+            {
+                return;
+            }
+
+            _time = default;
+            UpdateStopwatch();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _time += TimeSpan.FromMilliseconds(IntervalMilliseconds);
+            UpdateStopwatch();
+        }
+
+        private void UpdateStopwatch()
+        {
+            // Without data binding, any UI elements must be updated on the main thread (aka the 'UI thread').
+            // This is needed since Timer runs on the ThreadPool.
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Hours.Text = _time.Hours.ToString("00");
+                Minutes.Text = _time.Minutes.ToString("00");
+                Seconds.Text = _time.Seconds.ToString("00");
+                Milliseconds.Text = _time.Milliseconds.ToString("000");
+            });
         }
     }
 }
---
+++ Stopwatch/ViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.ComponentModel;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace Stopwatch
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private const int IntervalMilliseconds = 100;
+
+        private Timer _timer;
+        private TimeSpan _time;
+        private string _hours, _minutes, _seconds, _milliseconds;
+
+        public ViewModel()
+        {
+            Start = new Command(() =>
+            {
+                if (_timer != null)
+                {
+                    _timer.Stop();
+                    _timer.Elapsed -= Timer_Elapsed;
+                }
+
+                _timer = new Timer(IntervalMilliseconds);
+                _timer.Elapsed += Timer_Elapsed;
+                _timer.Start();
+            });
+
+            Stop = new Command(() =>
+            {
+                if (_timer is null)
+                {
+                    return;
+                }
+
+                _timer.Stop();
+            });
+
+            Reset = new Command(() =>
+            {
+                if (_timer is null)
+                {
+                    return;
+                }
+
+                _time = default;
+                UpdateStopwatch();
+            });
+        }
+
+        public string Hours
+        {
+            get => _hours;
+            private set
+            {
+                _hours = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Hours)));
+            }
+        }
+
+        public string Minutes
+        {
+            get => _minutes;
+            private set
+            {
+                _minutes = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Minutes)));
+            }
+        }
+
+        public string Seconds
+        {
+            get => _seconds;
+            private set
+            {
+                _seconds = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Seconds)));
+            }
+        }
+
+        public string Milliseconds
+        {
+            get => _milliseconds;
+            private set
+            {
+                _milliseconds = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Milliseconds)));
+            }
+        }
+
+        public Command Start { get; }
+        public Command Stop { get; }
+        public Command Reset { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _time += TimeSpan.FromMilliseconds(IntervalMilliseconds);
+            UpdateStopwatch();
+        }
+
+        private void UpdateStopwatch()
+        {
+            Hours = _time.Hours.ToString("00");
+            Minutes = _time.Minutes.ToString("00");
+            Seconds = _time.Seconds.ToString("00");
+            Milliseconds = _time.Milliseconds.ToString("000");
+        }
+    }
+}
